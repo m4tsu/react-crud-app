853f9859ed503aa56a664d798fb64ddb0f659f01
m4tsu <matsurec0147@gmail.com>
2019-08-24 18:58:44 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 5};
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1});
+  }
+  
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1});
+  }
+
   render() {
-    return (
-      <h1>
-        Hello, React!
-      </h1>
+    return(
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
     );
   }
 }

353632fee87af40c3f1d9292ff24a4749a8d3ae0
m4tsu <matsurec0147@gmail.com>
2019-08-25 21:29:58 +0900

    implement event_new

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,5 @@ export const READ_EVENTS = 'READ_EVENTS'
 
 export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
-  console.log(response)
   dispatch({type: READ_EVENTS, response})
 }
